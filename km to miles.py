5b403ae2a0f37adbf963ce3536d0f32edda8eb6c
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-06-19 22:09:34 +0530

    km to miles

---
+++ km to miles.py
@@ -0,0 +1,5 @@
+
+#1 kilometer = 0.6215 miles
+kilometers = float(input("Enter kilometer to convert into miles: "))
+miles = kilometers / 1.609
+print(f"{kilometers} kilometers are equal to {miles} miles")
